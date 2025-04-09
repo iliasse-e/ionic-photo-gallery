9d47d1d5ecea2cc6b18812ba1860c6c30209ebbd
Iliasse <elabouyi.iliasse@gmail.com>
2025-04-09 07:59:22 +0200

    add feature photo, and README

---
+++ README.md
@@ -0,0 +1,34 @@
+## PHOTO GALLERY App
+
+We'll create a Photo Gallery app that offers the ability to take photos with your device's camera, display them in a grid, and store them permanently on the device.
+
+Highlights include:
+
+- One Angular-based codebase that runs on the web, iOS, and Android using Ionic Framework [UI components](https://ionicframework.com/docs/components).
+- Deployed as a native iOS and Android mobile app using [Capacitor](https://capacitorjs.com), Ionic's official native app runtime.
+- Photo Gallery functionality powered by the Capacitor [Camera](https://capacitorjs.com/docs/apis/camera), [Filesystem](https://capacitorjs.com/docs/apis/filesystem), and [Preferences](https://capacitorjs.com/docs/apis/preferences) APIs.
+
+Find the complete app code referenced in this guide [on GitHub](https://github.com/ionic-team/photo-gallery-capacitor-ng).
+
+### Feat 1
+
+Add photos tab and gallery,
+Allow to take picture
+
+### Feat 2
+
+Store photos and load
+
+### Add mobile and deploy
+
+### Lifecycle hooks
+
+### Routing
+
+### Virtual scroll
+
+### Swiper.js
+
+### Plateform
+
+### Ionic storage
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { camera } from 'ionicons/icons';
+import { PhotoService } from '../services/photo.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [NgFor, IonRow, IonCol, IonImg, IonGrid, IonFab, IonFabButton, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) {
+    /**
+     * Any icons you want to use in your application
+     * can be registered in app.component.ts and then
+     * referenced by name anywhere in your application.
+     */
+    addIcons({camera})
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 
 }
